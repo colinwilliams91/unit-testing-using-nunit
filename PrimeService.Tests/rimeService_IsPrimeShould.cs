636b9fe4167b17e61f545eace4ba3c335d89e60b
colin <colin.williams.dev@gmail.com>
2024-01-05 16:48:23 -0600

    refactor: use TestCase for less repetitive tests, handle param, Assert That Is

--- PrimeService.Tests/rimeService_IsPrimeShould.cs
+++ PrimeService.Tests/rimeService_IsPrimeShould.cs
@@ -14,12 +14,14 @@ namespace Prime.UnitTests.Services
             _primeService = new PrimeService();
         }
 
-        [Test]
-        public void IsPrime_InputIs1_ReturnFalse()
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1)]
+        public void IsPrime_ValueLessThan2_ReturnFalse(int value)
         {
-            var result = _primeService.IsPrime(1);
+            var result = _primeService?.IsPrime(value);
 
-            Assert.IsFalse(result, "1 should not be prime");
+            Assert.That(result, Is.False, $"{value} should not be prime");
         }
     }
 }
