e0e06961ff38ecd33197891f4c174b700264561f
colin <colin.williams.dev@gmail.com>
2024-01-05 17:00:42 -0600

    feat: init DataTypeService class & make methods static fix, init IsInt method

---
+++ PrimeService/DataTypeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime.Services
+{
+    public class DataTypeService
+    {
+        public static bool IsInt (int value)
+        {
+            if (typeof value  != "number")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
