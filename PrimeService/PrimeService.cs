47bb6620533c8fa33e3db37893807175f8ead3e7
colin <colin.williams.dev@gmail.com>
2024-01-05 16:53:55 -0600

    fix: IsPrime service method handless less than 2 case

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -6,7 +6,7 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
-            if (candidate == 1) return false;
+            if (candidate < 2) return false;
 
             throw new NotImplementedException("Please create a test first.");
         }
