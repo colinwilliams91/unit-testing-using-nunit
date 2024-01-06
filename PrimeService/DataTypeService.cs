a555f9d144188a863e2e1b2ad421dd03b42be892
colin <colin.williams.dev@gmail.com>
2024-01-05 22:31:54 -0600

    feat: implement all type check methods

--- PrimeService/DataTypeService.cs
+++ PrimeService/DataTypeService.cs
@@ -8,9 +8,44 @@ namespace Prime.Services
 {
     public class DataTypeService
     {
-        public bool IsInt (object value)
+        public bool IsInt(object value)
         {
             return value is int;
+        }
+
+        public bool IsBoolean(object value)
+        {
+            return value is bool;
+        }
+
+        public bool IsString(object value)
+        {
+            return value is string;
+        }
+
+        public bool IsCharacter(object value)
+        {
+            return value is char;
+        }
+
+        public bool IsArray(object value)
+        {
+            return value.GetType() == typeof(Array);
+        }
+
+        public bool IsObj(object value)
+        {
+            return value is object;
+        }
+
+        public bool IsNaN(object value)
+        {
+            return value is double.NaN;
+        }
+
+        public bool IsNull(object value)
+        {
+            return value is null;
         }
     }
 }
