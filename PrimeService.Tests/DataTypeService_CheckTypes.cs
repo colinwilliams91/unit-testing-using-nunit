4e1330b5d8c5d547ab6779ff4d7afc774dcdc57e
colin <colin.williams.dev@gmail.com>
2024-01-05 20:56:07 -0600

    feat: DataTypes test init

--- PrimeService.Tests/DataTypeService_CheckTypes.cs
+++ PrimeService.Tests/DataTypeService_CheckTypes.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Prime.Services;
 
 namespace Prime.UnitTests.Services
 {
+    [TestFixture]
     public class DataTypeService_CheckTypes
     {
+        private DataTypeService _dataTypeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dataTypeService = new DataTypeService();
+        }
+
+        [Test]
+        public void CheckTypes()
+        {
+            var array = new int[] { 1, 2, 3 };
+
+            var obj = new Object;
+
+            var nan = double.NaN;
+        }
     }
 }
